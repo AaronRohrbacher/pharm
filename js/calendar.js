97b271dc2e2d45428a4f3452ea8d4760b95caf1b
Aaron Rohrbacher <arjazz@icloud.com>
2019-12-14 15:57:47 -0800

    add events, featured event to front page

--- js/calendar.js
+++ js/calendar.js
@@ -1,7 +1,6 @@
 var events = [
-  {'Date': new Date(2019, 10, 22), 'Title': 'GRAND RE-OPENING! | 5pm', 'Link': function(){$('#reopening').slideDown();}},
-  {'Date': new Date(2019, 11, 2), 'Title': 'Open Mic | 8:30pm', 'Link': function(){$('#openMic').slideDown();}},
-  {'Date': new Date(2019, 11, 6), 'Title': 'The City River Band | 8:30pm-11:30pm', 'Link': function(){$('#cityRiverBand').slideDown();}},
+  {'Date': new Date(2019, 11, 16), 'Title': 'Open Mic with Curtis Reynolds | 8:30pm', 'Link': function(){$('#openMic').slideDown();}},
+  {'Date': new Date(2019, 11, 30), 'Title': 'Open Mic with Curtis Reynolds | 8:30pm', 'Link': function(){$('#openMic').slideDown();}},
   {'Date': new Date(2019, 11, 31), 'Title': 'New Years Eve Party with The Bittersweet Symphonies | 9pm-1am', 'Link': function(){$('#newYears').slideDown();}},
 ];
 var settings = {};
